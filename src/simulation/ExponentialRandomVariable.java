7f6679d2bf43c6ee2bfe376922326e4245832ee0
Ahmed Kotb <kotbcorp@gmail.com>
2012-05-10 22:03:35 +0200

    Added Exponential random variable class

---
+++ src/simulation/ExponentialRandomVariable.java
@@ -0,0 +1,15 @@
+package simulation;
+
+public class ExponentialRandomVariable {
+
+	double rate = 0.0;
+	
+	public ExponentialRandomVariable(double rate){
+		this.rate = rate;
+	}
+	
+	public double generate(){
+		double u = Math.random();
+		return -1.0/rate * Math.log(u);
+	}
+}
