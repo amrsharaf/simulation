62009eddb215124a00e807f44e1f7c8d829d6366
Ahmed Kotb <kotbcorp@gmail.com>
2012-05-11 19:56:18 +0200

    implemented SeedGenerator

--- src/generators/LCG.java
+++ src/generators/LCG.java
@@ -1,56 +1,45 @@
 package generators;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class LCG {
+
 	long a;
 	long b;
 	long m;
 	long seed;
-	ArrayList<Long> randomNumbersPool;
-	
 
-	public LCG(long a, long b, long m , long seed) {
+	long last;
+
+	public LCG(long a, long b, long m, long seed) {
 		this.a = a;
 		this.b = b;
 		this.m = m;
 		this.seed = seed;
-		randomNumbersPool = new ArrayList<Long>();
-		randomNumbersPool.add(seed);
+		this.last = seed;
 	}
-	
-
 
 	public long getA() {
 		return a;
 	}
 
-
-
 	public void setA(long a) {
 		this.a = a;
 	}
-
-
 
 	public long getB() {
 		return b;
 	}
 
-
-
 	public void setB(long b) {
 		this.b = b;
 	}
 
-
-
 	public long getM() {
 		return m;
 	}
-
-
 
 	public void setM(long m) {
 		this.m = m;
@@ -60,61 +49,39 @@ public class LCG {
 		return seed;
 	}
 
-
-
 	public void setSeed(long seed) {
 		this.seed = seed;
 	}
-	
-	public long getNextRandom()
-	{
-		long x_n_1 = randomNumbersPool.get(randomNumbersPool.size()-1);
+
+	public long getNextRandom() {
+		long x_n_1 = last;
 		long x_n = -1;
-		if(b == 0)	//Multiplicative LCG
-		{
-			x_n = g(x_n_1) + m*h(x_n_1);
+		if (b == 0) {	// Multiplicative LCG
+			x_n = g(x_n_1) + m * h(x_n_1);
+		} else {	// Mixed LCG
+			x_n = (a * x_n_1 + b) % m;
 		}
-		else	//Mixed LCG
-		{
-			x_n = (a*x_n_1 + b) % m;
-		}
-		randomNumbersPool.add(x_n);	//save at pool
-		return x_n;		
+		last = x_n;
+		return x_n;
 	}
-	
-	public List<Long> getNextRandoms(int n)
-	{
-		for (int i = 0; i < n; i++) 
-		{
-			getNextRandom();
-		}
-		return  randomNumbersPool.subList(randomNumbersPool.size()-n, randomNumbersPool.size());
-	}
-	
-	private long g(long x)
-	{
+
+	private long g(long x) {
 		long q = m / a;
 		long r = m % a;
-		return ( a*(x % q) ) - ( r*(x / q) );
+		return (a * (x % q)) - (r * (x / q));
 	}
-	
-	private long h(long x)
-	{
+
+	private long h(long x) {
 		long q = m / a;
-		return (x / q) - ( (a*x) / m);
+		return (x / q) - ((a * x) / m);
 	}
-
-
 
 	public static void main(String[] args) {
-		LCG rnd = new LCG(16807,0,2147483647,1);
-		/*for (int i = 0; i < 20; i++) {
-			System.out.print(rnd.getNextRandom() +", "); 
+		LCG rnd = new LCG(16807, 0, 2147483647, 1);
+		for (int i = 0; i < 20; i++) {
+			System.out.println(rnd.getNextRandom());
+			
 		}
-		System.out.println();
-		*/
-		System.out.println(rnd.getNextRandoms(100000));
-
 	}
 
 }
---
+++ src/generators/SeedGenerator.java
@@ -0,0 +1,55 @@
+package generators;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class SeedGenerator {
+
+	LCG lcg;
+	ArrayList<Long> seeds;
+	final int LCG_INTERVAL = 100000;
+	int index = 0;
+	
+	public SeedGenerator() {
+		lcg = new LCG(16807, 0, 2147483647, 1);
+		init();
+	}
+	
+	public void init(){
+		seeds = new ArrayList<Long>();
+		seeds.add(1L);
+		int count = 1;
+		long num;
+		for (int i = 0; i < 2147483647; i++) {
+			num = lcg.getNextRandom();
+			if (count == 0)
+				seeds.add(num);
+			
+			count +=1;
+			count %= LCG_INTERVAL;
+			if (seeds.size() == 10) break;
+		}
+		
+		System.out.println(seeds.size());
+		System.out.println(seeds);
+	}
+	
+	public long getNextSeed(){
+		return seeds.get(index++);
+	}
+	
+	public static void main(String[] args){
+		SeedGenerator s =  new SeedGenerator();
+		long s1 = s.getNextSeed();
+		long s2 = s.getNextSeed();
+		LCG lcg1 = new LCG(16807, 0, 2147483647, s1);
+		LCG lcg2 = new LCG(16807, 0, 2147483647, s2);
+		HashSet<Long> set = new HashSet<Long>();
+		for (int i = 0; i < 100000; i++) {
+			set.add(lcg1.getNextRandom());
+			set.add(lcg2.getNextRandom());
+		}
+		//size should be 20000 as numbers shouldn't overlab
+		System.out.println(set.size());
+	}
+}
