20a7ddd9a3b6689f6bc1adb159a6fa032d107752
Ahmed Kotb <kotbcorp@gmail.com>
2012-05-17 01:14:22 +0200

    added lcg images

---
+++ analysis/1/sample.m
@@ -0,0 +1,23 @@
+x = [16807, 282475249, ....]
+
+%draw histogram
+k = 100;
+hist(x,k)
+h = findobj(gca, 'Type','patch');
+set(h(1), 'FaceColor','k', 'EdgeColor','r')
+
+o = hist(x,k);
+disp(o)
+%get length of samples
+n = length(x);
+
+%calc expected value
+e = n/k;
+
+%calc D = segma((o_i -e)^2/e)
+D = 0;
+for i = 1:k
+  D = D + ( ( (o(i) - e)^2 ) / e );
+end 
+fprintf('D = %d\n',D);
+
